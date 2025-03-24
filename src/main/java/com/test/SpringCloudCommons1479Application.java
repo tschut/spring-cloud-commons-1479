ab2b1ed54c350f0b773dd824e16c6ffefc490986
Tiemen Schut <tschut@bol.com>
2025-03-24 10:11:25 +0100

    Initial commit

---
+++ src/main/java/com/test/SpringCloudCommons1479Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCommons1479Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudCommons1479Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/SpringCloudCommons1479ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCommons1479ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
